ea33f6ae86b8a530eefe9ff5351096b244972757
bimaryan <bimaryan046@gmail.com>
2023-02-07 08:55:26 +0700

    update

---
+++ about.php
@@ -0,0 +1,12 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-header text-center">
+                About
+            </div>
+            <div class="card-body">
+                <?php include "about.html" ?>
+            </div>
+        </div>
+    </div>
+</section>
